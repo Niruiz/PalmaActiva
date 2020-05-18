365ca507b84c1031499ba53b566e19ea451dea02
Niruiz <43117479+Niruiz@users.noreply.github.com>
2020-05-19 01:51:03 +0200

    Create coche.js

---
+++ js/coche.js
@@ -0,0 +1,20 @@
+function nombre() {
+    var nombre = prompt("Escribe tu nombre");
+    document.getElementById("nombre").innerHTML = nombre;
+}
+
+function coche() {
+    var modelos = [];
+    var cont = "";
+    for (var i = 0; i <= 2; i++) {
+        var x = prompt("Ingresa el modelo de tu coche:");
+        modelos[i] = x;
+    }
+    for (var i = 0; i <= 2; i++) {
+        cont += modelos[i] + " ";
+    }
+    document.getElementById("modelos").innerHTML = cont;
+}
+
+nombre();
+coche();
